49eb42bea89c9d02d10838b61b3e644766531e37
CoudronLukas <lukas.coudron@student.howest.be>
2021-12-22 12:28:35 +0100

    Laatste push

--- EXAMEN/Voor deel 3/Deel3.sh
+++ EXAMEN/Voor deel 3/Deel3.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 if [ "$(id -u)" -ne 0 ]; then
-
   printf "dit script moet gerunt worden door de root!\n\n"
-
   exit 1
-
 fi
 
 #hostname=
@@ -14,13 +11,14 @@ input="/home/lukas/Documenten/Examen/LVL-2-POE-config.txt"
 #Wanneer een trunk gevonden is zal hij de VLAN's aanmaken anders zal hij gewoon verder doen
 while IFS= read -r line
 do
-	if [ $line ?? 'switchport trunk allowed vlan' ]; then
+	if [[ $line == *'switchport trunk allowed vlan'* ]]; then
 	  echo "Trunk gevonden..."
 	  echo "Vlan's Aanmaken!"
-	  sed $line "switchport trunk allowed vlan "
-	  
-	else
-		printf "$line\n" >> /home/lukas/Documenten/Examen/debian-test.txt
+	  printf "$line"
+	  printf "$line" | sed "switchport trunk allowed vlan " >> /home/lukas/Documenten/Examen/debian-test.txt
+
+	else # De rest van de code uitprinten
+	  printf "$line\n" >> /home/lukas/Documenten/Examen/debian-test.txt
 	fi
   #printf "$line\n" >> $hostname"-"$ipvlan1.txt
 done < "$input"
